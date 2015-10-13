ed1d429c979caae33958f84c55a52dd210ece053
3disqd <kirill.sidorko@ya.ru>
2015-10-13 05:19:03 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ var phoneBook = require('./phoneBook');
 phoneBook.add('Сергей', '7 999 6667778', 'gs@example.com');
 phoneBook.add('Сергей 2', '999 4433444', 'gs@example.com');
 phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@yandex-team.ru');
+phoneBook.add('Олег', '+7 (999) 777-7-777', 'just7@ya223ndex.ru');
 
 // Невалидные данные не должны попадать в книгу!
 phoneBook.add('Честный Хрюндель', 'invalid phone', 'honest-hrundel');
+// Валидный номер невалидная почта
+phoneBook.add('3disqd', '1234567', 'pochta@ya.ru');
 
-phoneBook.find('777');
+//phoneBook.find('3dis');
+//phoneBook.find('777');
 // Выводит построчно записи, все поля через запятую:
 // Сергей, +7 (999) 666-7-778, gogolef@yandex-team.ru
 // Олег, +7 (999) 777-7-777, just7@yandex-team.ru
-
+phoneBook.showTable();
 phoneBook.remove('Олег');
 // Выводит количество удалённых контактов, которые удовлетворят запросу:
 // Удален 1 контакт
@@ -32,4 +36,5 @@ phoneBook.showTable();
 
 // Экспортируем записи, пример файла рядом
 phoneBook.importFromCsv('./backup.csv');
+phoneBook.showTable();
 // Добавлено 4 контакта
